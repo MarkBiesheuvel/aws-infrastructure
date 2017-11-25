dd2e52cac2bd9fac95e8024810020cfce64945a8
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-11-25 12:41:19 +0100

    Add more outputs from vpc module

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,3 +1,7 @@
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
 output "private_ipv4_cidr_ranges" {
   value = "${zipmap(aws_subnet.private_subnets.*.availability_zone, aws_subnet.private_subnets.*.cidr_block)}"
 }
@@ -6,10 +10,18 @@ output "private_ipv6_cidr_ranges" {
   value = "${zipmap(aws_subnet.private_subnets.*.availability_zone, aws_subnet.private_subnets.*.ipv6_cidr_block)}"
 }
 
+output "private_subnet_ids" {
+  value = "${zipmap(aws_subnet.private_subnets.*.availability_zone, aws_subnet.private_subnets.*.id)}"
+}
+
 output "public_ipv4_cidr_ranges" {
   value = "${zipmap(aws_subnet.public_subnets.*.availability_zone, aws_subnet.public_subnets.*.cidr_block)}"
 }
 
 output "public_ipv6_cidr_ranges" {
   value = "${zipmap(aws_subnet.public_subnets.*.availability_zone, aws_subnet.public_subnets.*.ipv6_cidr_block)}"
+}
+
+output "public_subnet_ids" {
+  value = "${zipmap(aws_subnet.public_subnets.*.availability_zone, aws_subnet.public_subnets.*.id)}"
 }
--- modules/vpc/resources.tf
+++ modules/vpc/resources.tf
@@ -111,33 +111,3 @@ resource "aws_route_table_association" "public_subnet_route_table" {
   subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
   route_table_id = "${aws_route_table.public_route_table.id}"
 }
-
-resource "aws_security_group" "ssh" {
-  name        = "ssh-access"
-  description = "Allow SSH access"
-  vpc_id      = "${aws_vpc.vpc.id}"
-
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = -1
-    self      = true
-  }
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    # TODO: create kms key to encrypt own IP
-    cidr_blocks = ["0.0.0.0/32"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
