8db96316c5a88d07093b55f8ce050690982d6173
Mark Biesheuvel <mail@markbiesheuvel.nl>
2017-12-09 13:49:08 +0100

    Switch instance type now t2 supports spot pricing

--- modules/elasticsearch/instance.tf
+++ modules/elasticsearch/instance.tf
@@ -35,11 +35,11 @@ resource "aws_spot_instance_request" "elasticsearch" {
   }
 
   ami                  = "${data.aws_ami.amazon_linux.id}"
-  instance_type        = "i3.large"
+  instance_type        = "t2.small"
   key_name             = "${aws_key_pair.elasticsearch.key_name}"
   subnet_id            = "${lookup(var.subnet_ids, data.aws_availability_zones.available.names[0])}"
   user_data            = "${data.template_file.user_data.rendered}"
-  spot_price           = "0.027"
+  spot_price           = "0.0268"
   spot_type            = "persistent"
   iam_instance_profile = "${aws_iam_instance_profile.elasticsearch.name}"
   wait_for_fulfillment = true
