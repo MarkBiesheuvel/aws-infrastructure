c80cdba742a28c24785b328c568dcf9692e1baaf
Mark Biesheuvel <mail@markbiesheuvel.nl>
2018-10-19 14:54:09 +0200

    Make use of remote state

---
+++ modules/remote_state_storage/dynamodb.tf
@@ -0,0 +1,19 @@
+resource "aws_dynamodb_table" "lock_table" {
+  name           = "terraform-lock"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+}
---
+++ remote_state.tf
@@ -0,0 +1,13 @@
+
+module "remote_state_storage" {
+  source         = "modules/remote_state_storage"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "markbiesheuvel-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
