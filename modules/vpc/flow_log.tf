2f8b38b3cc5e687bd2d668b6d7f5510ad63fbb6a
Mark Biesheuvel <mail@markbiesheuvel.nl>
2019-01-21 13:52:29 +0100

    Update deprecated parameter

--- modules/vpc/flow_log.tf
+++ modules/vpc/flow_log.tf
@@ -1,8 +1,9 @@
 resource "aws_flow_log" "vpc_flow_log" {
-  iam_role_arn    = "${aws_iam_role.vpc_flow_log.arn}"
-  log_group_name  = "${aws_cloudwatch_log_group.vpc_flow_log.name}"
-  traffic_type    = "ALL"
-  vpc_id          = "${aws_vpc.vpc.id}"
+  iam_role_arn         = "${aws_iam_role.vpc_flow_log.arn}"
+  log_destination      = "${aws_cloudwatch_log_group.vpc_flow_log.arn}"
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+  vpc_id               = "${aws_vpc.vpc.id}"
 }
 
 resource "aws_cloudwatch_log_group" "vpc_flow_log" {
