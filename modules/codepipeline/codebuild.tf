c9f28327541f65548cf56bf3ad96acdb80911ac3
Mark Biesheuvel <mail@markbiesheuvel.nl>
2019-08-14 21:38:48 +0200

    Update CodeBuild projects

--- modules/codepipeline/codebuild.tf
+++ modules/codepipeline/codebuild.tf
@@ -91,7 +91,7 @@ resource "aws_codebuild_project" "main" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/nodejs:8.11.0"
+    image        = "aws/codebuild/standard:2.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
